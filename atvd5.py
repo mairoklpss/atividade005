52ef6d485f06554ad3dd2e60ad366834d94b711d
mairoklpss <mairalopesma@gmail.com>
2024-09-04 15:41:28 -0300

    conversão de min para seg

---
+++ atvd5.py
@@ -0,0 +1,5 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+min = int(input("quantos minutos? "))
+seg = min*60
+
+print(f"{min} minutos em segundos é {seg} segundos.")
